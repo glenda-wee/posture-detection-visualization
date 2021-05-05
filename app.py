89d9c9a12c45b16b6715bdd7bbca1e5ce12cb028
Glenda <glenda_wee@mymail.sutd.edu.sg>
2021-05-06 02:34:30 +0800

    idk how to do the frequency

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ df_logs = pd.read_csv("posture_detection_logs.csv")
 #calculating frequency of toilet use 
 df_frequency = pd.DataFrame(columns = ["Time", "Frequency"])
 
+
 #calculating number of toilet visits per day 
 df_average_visits = df_logs.groupby("bed_number").count()
 df_average_visits = df_average_visits.iloc[:, 0:1]
@@ -29,31 +30,34 @@ df_hfr_count.reset_index(inplace = True)
 
 with header: 
     st.title("Ward 37's HFR Patients")
-    st.text("Creating the dashboard with fake data")
+    st.write("Creating the dashboard with fake data")
 
 with dataset: 
     st.header("The Dataset")
-    st.text("These are the logs for HFR patients.")
+    st.write("These are the logs for HFR patients.")
     st.write(df_logs)
 
 with ward_layout: 
     st.header("Ward 37's Layout")
-    st.text("This is the layout of Ward 37.")
+    st.write("This is the layout of Ward 37.")
     st.image("ward_layout.png")
 
 with frequency: 
     st.header("Frequency of Toilet Use")
-    st.text("This bar graph shows the average number of patients who use the toilet at that hour.")
+    st.write("This histogram shows the average number of patients who use the toilet at that hour.")
+    st.write("Calculated by taking the starting timestamp.")
     st.write(df_frequency)
 
 with average_visits: 
     st.header("Average Number of Toilet Visits Per Day")
-    st.text("This table shows the average number times each patient goes to the toilet.")
+    st.write("This table shows the average number times each patient goes to the toilet.")
     st.write(df_average_visits)
 
 with hfr_count: 
     st.header("Patients By Their HFR Counts")
-    st.text("This table shows the total number of times that a HFR position was detected in this patient since they were warded.")
+    st.write("This table shows the total number of times that a HFR position was detected in this patient since they were warded.")
     st.write(df_hfr_count)
 
 st.balloons()
+
+
